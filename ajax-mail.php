a3fa7ca1a7103dd5a5f4c0051cb7e8dd547fcb59
kzkick2nd <aruga.kazuki@gmail.com>
2017-11-09 02:32:49 +0900

    proto type

--- ajax-mail.php
+++ ajax-mail.php
@@ -1,1 +1,15 @@
 <?php
+  $data = $_POST;
+
+  mb_language("Japanese");
+  mb_internal_encoding("UTF-8");
+
+  $to      = '';
+  $subject = 'ajax-mail';
+  $message = $data['body'];
+  $headers = 'From: ' . $data['email'] . "\r\n";
+
+  mb_send_mail($to, $subject, $message, $headers);
+
+  header('Content-Type: application/json');
+  echo json_encode( "mail sent." );
