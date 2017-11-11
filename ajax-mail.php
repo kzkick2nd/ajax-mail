062fa12c51dceab584db9619c9f9c4a164fd4135
kzkick2nd <aruga.kazuki@gmail.com>
2017-11-12 06:50:11 +0900

    Add validating request

--- ajax-mail.php
+++ ajax-mail.php
@@ -1,15 +1,21 @@
 <?php
-  $data = $_POST;
-
-  mb_language("Japanese");
-  mb_internal_encoding("UTF-8");
-
-  $to      = '';
-  $subject = 'ajax-mail';
-  $message = $data['body'];
-  $headers = 'From: ' . $data['email'] . "\r\n";
-
-  mb_send_mail($to, $subject, $message, $headers);
-
   header('Content-Type: application/json');
-  echo json_encode( "mail sent." );
+  if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+    && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest'
+    && isset($_POST))
+  {
+    $data = $_POST;
+
+    $to      = '';
+    $subject = 'ajax-mail';
+    $message = $data['body'];
+    $headers = 'From: ' . $data['email'] . "\r\n";
+
+    mb_language("Japanese");
+    mb_internal_encoding("UTF-8");
+    mb_send_mail($to, $subject, $message, $headers);
+
+    echo json_encode( "mail sent." );
+  }else{
+    http_response_code(403);
+  }
